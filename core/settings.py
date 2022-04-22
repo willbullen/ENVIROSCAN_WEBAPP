a866ced29fb43283fbeb119e8af07b0b184e82d8
willbullen <williambullen@gmail.com>
2022-04-22 13:24:06 +0100

    lll

--- core/settings.py
+++ core/settings.py
@@ -178,22 +178,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.normpath(os.path.join(CORE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where uploaded media is saved.
+MEDIA_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'media')) # Directory where uploaded media is saved.
 MEDIA_URL = '/media/' # Public URL at the browser
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(CORE_DIR, 'core/static'),
+    os.path.normpath(os.path.join(CORE_DIR, 'core/static')),
+    #os.path.join(CORE_DIR, 'core/static'),
 )
 #############################################################
 ## CELERY BEATS
-CELERY_BROKER_URL = 'redis://:Po0AuJljh9cvvuiUWFeqZFOpJde3SsCVHAzCaCbvplw=@enviroscan.redis.cache.windows.net:6379/0'
-CELERY_RESULT_BACKEND = 'redis://:Po0AuJljh9cvvuiUWFeqZFOpJde3SsCVHAzCaCbvplw=@enviroscan.redis.cache.windows.net:6379/0'
+#CELERY_BROKER_URL = 'redis://:Po0AuJljh9cvvuiUWFeqZFOpJde3SsCVHAzCaCbvplw=@enviroscan.redis.cache.windows.net:6379/0'
+#CELERY_RESULT_BACKEND = 'redis://:Po0AuJljh9cvvuiUWFeqZFOpJde3SsCVHAzCaCbvplw=@enviroscan.redis.cache.windows.net:6379/0'
 
-CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+#CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 #############################################################
 
