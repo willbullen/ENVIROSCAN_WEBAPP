a13f4a68f4467d2a8e4c6545dafe85fb305449bb
willbullen <williambullen@gmail.com>
2022-07-14 12:05:31 +0100

    new

--- core/static/assets/js/picarro.js
+++ core/static/assets/js/picarro.js
@@ -153,7 +153,7 @@ function setSetup(db_Data) {
 var startWebSocket = function () {
     var picarroSocket;
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    var ws_path = ws_scheme + '://' + window.location.host + '/picarro/';
+    var ws_path = ws_scheme + '://' + window.location.host + ':8001/picarro/';
     picarroSocket = new WebSocket(ws_path);    
 
     picarroSocket.onmessage = function (e) {
