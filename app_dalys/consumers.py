664517843a69f9747db318b2d8154688bb945dc7
willbullen <williambullen@gmail.com>
2022-07-18 11:33:18 +0100

    c

--- app_dalys/consumers.py
+++ app_dalys/consumers.py
@@ -27,7 +27,9 @@ class DalysConsumer(AsyncWebsocketConsumer):
         )
 
     async def receive(self, text_data):
-        json_data = json.loads(text_data)        
+        json_data = json.loads(text_data)
+        # tag on history data
+        json_data['History'] = await json.loads(Node_Power.objects.filter(Node = json_data['Node_ID']).order_by('-id')[:1440].to_dataframe(index='Data_DateTime').sort_index(ascending=True).resample('10Min').mean().fillna(method='backfill').tail(140).to_json(orient="table"))
         # Send message to group
         await self.channel_layer.group_send(
             self.group_name,
@@ -36,21 +38,7 @@ class DalysConsumer(AsyncWebsocketConsumer):
                 'message': json_data
             }
         )
-        # Send history to group               
-        if json_data['Action'] == 'Heartbeat':
-            # GET NODE DETAILS
-            node_id = json_data['Node_ID']
-            # get history data
-            json_history_data = await get_data.get_history_data(Node_Power, node_id)
-            # GET SEND DATA
-            await self.channel_layer.group_send(
-                self.group_name,
-                {
-                    'type': 'stream.message',
-                    'message': json_history_data
-                }
-        )
-
+        
     # Receive message from room group
     async def stream_message(self, event):
         message = event['message']
@@ -64,7 +52,7 @@ class get_data:
     async def get_history_data(object, node_id):
         json_history_data = {}
         try:            
-            json_history_data['Data'] = await json.loads(object.objects.filter(Node = node_id).order_by('-id')[:1440].to_dataframe(index='Data_DateTime').sort_index(ascending=True).resample('10Min').mean().fillna(method='backfill').tail(140).to_json(orient="table"))
+            json_history_data['HIstory'] = await json.loads(object.objects.filter(Node = node_id).order_by('-id')[:1440].to_dataframe(index='Data_DateTime').sort_index(ascending=True).resample('10Min').mean().fillna(method='backfill').tail(140).to_json(orient="table"))
             json_history_data['Data_Type'] = await "History_Data"
             json_history_data['Node_ID'] = await node_id            
         except Exception as e:
