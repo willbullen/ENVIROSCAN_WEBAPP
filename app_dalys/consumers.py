f764a40e20846cfa89bef8343a52a340f16f96f3
willbullen <williambullen@gmail.com>
2022-07-18 12:11:46 +0100

    lll

--- app_dalys/consumers.py
+++ app_dalys/consumers.py
@@ -5,6 +5,7 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer, SyncConsumer, WebsocketConsumer, JsonWebsocketConsumer
 from .models import Node_Power
 from channels.layers import get_channel_layer
+from channels.db import database_sync_to_async
 
 class DalysConsumer(AsyncWebsocketConsumer):
     # SET VARIABLES
@@ -29,7 +30,8 @@ class DalysConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         json_data = json.loads(text_data)
         # tag on history data
-        json_data['History'] = await json.loads(Node_Power.objects.filter(Node = json_data['Node_ID']).order_by('-id')[:1440].to_dataframe(index='Data_DateTime').sort_index(ascending=True).resample('10Min').mean().fillna(method='backfill').tail(140).to_json(orient="table"))
+        history = await database_sync_to_async(self.get_history_data(Node_Power, json_data['Node_ID']))()
+        json_data['History'] = json.loads(history)
         # Send message to group
         await self.channel_layer.group_send(
             self.group_name,
@@ -47,14 +49,6 @@ class DalysConsumer(AsyncWebsocketConsumer):
             'message': message
         }))
 
-class get_data:
     # get history data
-    async def get_history_data(object, node_id):
-        json_history_data = {}
-        try:            
-            json_history_data['HIstory'] = await json.loads(object.objects.filter(Node = node_id).order_by('-id')[:1440].to_dataframe(index='Data_DateTime').sort_index(ascending=True).resample('10Min').mean().fillna(method='backfill').tail(140).to_json(orient="table"))
-            json_history_data['Data_Type'] = await "History_Data"
-            json_history_data['Node_ID'] = await node_id            
-        except Exception as e:
-            print('{!r}; Get History data failed - '.format(e))
-        return json_history_data
+    def get_history_data(object, node_id):
+        return object.objects.filter(Node = node_id).order_by('-id')[:1440].to_dataframe(index='Data_DateTime').sort_index(ascending=True).resample('10Min').mean().fillna(method='backfill').tail(140).to_json(orient="table")
