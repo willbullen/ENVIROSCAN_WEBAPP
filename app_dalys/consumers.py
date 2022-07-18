d04e4fb71e815538c044a13c68a0c8063c037634
willbullen <williambullen@gmail.com>
2022-07-18 12:40:20 +0100

    www

--- app_dalys/consumers.py
+++ app_dalys/consumers.py
@@ -5,50 +5,49 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer, SyncConsumer, WebsocketConsumer, JsonWebsocketConsumer
 from .models import Node_Power
 from channels.layers import get_channel_layer
-from channels.db import database_sync_to_async
 
-class DalysConsumer(AsyncWebsocketConsumer):
+class DalysConsumer(WebsocketConsumer):
     # SET VARIABLES
     group_name = 'dalys'
     channel_name = ''
 
-    async def connect(self):
-        # Join group
-        await self.channel_layer.group_add(
+    def async_send(self, channel_name, jsonData):
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(channel_name, {"type": "stream.message", 'message': jsonData})
+
+    def connect(self):
+        # JOIN GROUP
+        async_to_sync(self.channel_layer.group_add)(
             self.group_name,
             self.channel_name
         )
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
-        # Leave group
-        await self.channel_layer.group_discard(
+    def disconnect(self, close_code):
+        # leave group
+        async_to_sync(self.channel_layer.group_discard)(
             self.group_name,
             self.channel_name
         )
 
-    async def receive(self, text_data):
+    def receive(self, text_data):
         json_data = json.loads(text_data)
-        # tag on history data
-        history = await database_sync_to_async(self.get_history_data(Node_Power, json_data['Node_ID']))()
-        json_data['History'] = json.loads(history)
-        # Send message to group
-        await self.channel_layer.group_send(
-            self.group_name,
-            {
-                'type': 'stream.message',
-                'message': json_data
-            }
-        )
-        
-    # Receive message from room group
-    async def stream_message(self, event):
-        message = event['message']
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        if json_data['Action'] == 'Heartbeat':
+            # tag history onto data
+            json_data['History'] = Get_Data.get_history_data(Node_Power, json_data['Node_ID'])
+            # send it
+            self.async_send(self.group_name, json_data)
 
-    # get history data
-    def get_history_data(self, object, node_id):
-        return object.objects.filter(Node = node_id).order_by('-id')[:1440].to_dataframe(index='Data_DateTime').sort_index(ascending=True).resample('10Min').mean().fillna(method='backfill').tail(140).to_json(orient="table")
+    def stream_message(self, event):
+        message = event['message']
+        self.send(text_data=json.dumps({'message': message}))
+
+class Get_Data:
+
+    def get_history_data(object, node_id):
+        history = {}
+        try:            
+            history = json.loads(object.objects.filter(Node = node_id).order_by('-id')[:1440].to_dataframe(index='Data_DateTime').sort_index(ascending=True).resample('10Min').mean().fillna(method='backfill').tail(140).to_json(orient="table"))          
+        except Exception as e:
+            print('{!r}; Get History data failed - '.format(e))
+        return history
