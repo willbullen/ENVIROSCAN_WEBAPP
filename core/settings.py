b31c2f9ac2629b3c632c7ff528906bbc350f4540
willbullen <williambullen@gmail.com>
2023-04-23 10:38:12 +0100

    dd

--- core/settings.py
+++ core/settings.py
@@ -50,8 +50,8 @@ INSTALLED_APPS = [
     'app_meteireann',
 
     'django.contrib.sites',         # Required for determining domain url for use in emails
-    #'django.contrib.humanize',      # Required for elapsed time formatting
-    #'bootstrap4form',               # Required for nicer formatting of forms with the default templates
+    'django.contrib.humanize',      # Required for elapsed time formatting
+    'bootstrap4form',               # Required for nicer formatting of forms with the default templates
     #'account',                      # Required by pinax-teams
     #'pinax.invitations',            # Required by pinax-teams
     #'pinax.teams',                  # Team support
