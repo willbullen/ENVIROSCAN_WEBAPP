c398d1a0bd40bfdd73eec7185565db778d9c3b3f
willbullen <williambullen@gmail.com>
2023-04-23 10:18:09 +0100

    lll

--- core/settings.py
+++ core/settings.py
@@ -49,14 +49,14 @@ INSTALLED_APPS = [
     'app_cuh',
     'app_meteireann',
 
-    #'django.contrib.sites',  # Required for determining domain url for use in emails
-    #'django.contrib.humanize',  # Required for elapsed time formatting
-    #'bootstrap4form', # Required for nicer formatting of forms with the default templates
-    #'account',  # Required by pinax-teams
-    #'pinax.invitations',  # Required by pinax-teams
-    #'pinax.teams',  # Team support
-    #'reversion',  # Required by pinax-teams
-    #'helpdesk',  # This is us!
+    'django.contrib.sites',         # Required for determining domain url for use in emails
+    'django.contrib.humanize',      # Required for elapsed time formatting
+    'bootstrap4form',               # Required for nicer formatting of forms with the default templates
+    'account',                      # Required by pinax-teams
+    'pinax.invitations',            # Required by pinax-teams
+    'pinax.teams',                  # Team support
+    'reversion',                    # Required by pinax-teams
+    #'helpdesk',                    # This is us!
 ]
 
 MIDDLEWARE = [
